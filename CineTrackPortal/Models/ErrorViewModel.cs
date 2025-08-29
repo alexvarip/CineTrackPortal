c40582668545ed071f1579b5f70f40aa9dd8641c
alex varypatis <alexvarip@windowslive.com>
2025-08-29 22:49:04 +0300

    Add project files.

---
+++ CineTrackPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CineTrackPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
