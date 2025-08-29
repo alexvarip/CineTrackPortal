59fa99883d39569b9ec6dea0e5d5bafc5f18dfd7
alex varypatis <alexvarip@windowslive.com>
2025-08-30 01:10:35 +0300

    Update ApplicationDBContext to include DbSet of Actor and Movie Models.
    Inlcude seed sample data for Actor and Movie Models.
    
    Add InitialCreate migration and update database.

--- CineTrackPortal/Data/ApplicationDbContext.cs
+++ CineTrackPortal/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CineTrackPortal.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CineTrackPortal.Data
@@ -9,5 +10,75 @@ namespace CineTrackPortal.Data
             : base(options)
         {
         }
+
+        public DbSet<ActorModel> Actors { get; set; }
+        public DbSet<MovieModel> Movies { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Many-to-many relationship between Movies and Actors
+            modelBuilder.Entity<MovieModel>()
+                .HasMany(m => m.Actors)
+                .WithMany(a => a.Movies);
+
+
+            // Seed Sample Actors
+            modelBuilder.Entity<ActorModel>().HasData(
+                new ActorModel 
+                {
+                    ActorId = Guid.Parse("00000000-0000-0000-0000-000000000001"), 
+                    FirstName = "Tom",
+                    LastName = "Hanks" 
+                },
+                new ActorModel 
+                {
+                    ActorId = Guid.Parse("00000000-0000-0000-0000-000000000002"), 
+                    FirstName = "Meryl", 
+                    LastName = "Streep" 
+                }
+            );
+
+
+            // Seed Sample Movies
+            modelBuilder.Entity<MovieModel>().HasData(
+                new MovieModel 
+                { 
+                    MovieId = Guid.Parse("00000000-0000-0000-1111-000000000001"),
+                    Title = "Forrest Gump", 
+                    Date = new DateTime(1994, 7, 6) },
+                new MovieModel 
+                { 
+                    MovieId = Guid.Parse("00000000-0000-0000-1111-000000000002"), 
+                    Title = "The Post",
+                    Date = new DateTime(2017, 12, 22) 
+                }
+            );
+
+
+            // Seed Movie-Actor relationships (join table)
+            modelBuilder.Entity<MovieModel>()
+                .HasMany(m => m.Actors)
+                .WithMany(a => a.Movies)
+                .UsingEntity(j => j.HasData(
+                    new {
+                        MoviesMovieId = Guid.Parse("00000000-0000-0000-1111-000000000001"),
+                        ActorsActorId = Guid.Parse("00000000-0000-0000-0000-000000000001") }, // Forrest Gump - Tom Hanks
+                    new {
+                        MoviesMovieId = Guid.Parse("00000000-0000-0000-1111-000000000002"), 
+                        ActorsActorId = Guid.Parse("00000000-0000-0000-0000-000000000001") }, // The Post - Tom Hanks
+                    new { 
+                        MoviesMovieId = Guid.Parse("00000000-0000-0000-1111-000000000002"),
+                        ActorsActorId = Guid.Parse("00000000-0000-0000-0000-000000000002") }  // The Post - Meryl Streep
+                ));
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+
     }
 }
---
+++ CineTrackPortal/Data/Migrations/20250829220530_InitialCreate.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CineTrackPortal.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actors",
+                columns: table => new
+                {
+                    ActorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors", x => x.ActorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MovieId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MovieId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ActorModelMovieModel",
+                columns: table => new
+                {
+                    ActorsActorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MoviesMovieId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActorModelMovieModel", x => new { x.ActorsActorId, x.MoviesMovieId });
+                    table.ForeignKey(
+                        name: "FK_ActorModelMovieModel_Actors_ActorsActorId",
+                        column: x => x.ActorsActorId,
+                        principalTable: "Actors",
+                        principalColumn: "ActorId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ActorModelMovieModel_Movies_MoviesMovieId",
+                        column: x => x.MoviesMovieId,
+                        principalTable: "Movies",
+                        principalColumn: "MovieId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "ActorId", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { new Guid("00000000-0000-0000-0000-000000000001"), "Tom", "Hanks" },
+                    { new Guid("00000000-0000-0000-0000-000000000002"), "Meryl", "Streep" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "Date", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("00000000-0000-0000-1111-000000000001"), new DateTime(1994, 7, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), "Forrest Gump" },
+                    { new Guid("00000000-0000-0000-1111-000000000002"), new DateTime(2017, 12, 22, 0, 0, 0, 0, DateTimeKind.Unspecified), "The Post" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ActorModelMovieModel",
+                columns: new[] { "ActorsActorId", "MoviesMovieId" },
+                values: new object[,]
+                {
+                    { new Guid("00000000-0000-0000-0000-000000000001"), new Guid("00000000-0000-0000-1111-000000000001") },
+                    { new Guid("00000000-0000-0000-0000-000000000001"), new Guid("00000000-0000-0000-1111-000000000002") },
+                    { new Guid("00000000-0000-0000-0000-000000000002"), new Guid("00000000-0000-0000-1111-000000000002") }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActorModelMovieModel_MoviesMovieId",
+                table: "ActorModelMovieModel",
+                column: "MoviesMovieId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActorModelMovieModel");
+
+            migrationBuilder.DropTable(
+                name: "Actors");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
