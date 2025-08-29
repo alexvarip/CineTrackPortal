c4576cd1a9ade54e3bc1393373c262412fe90bf2
alex varypatis <alexvarip@windowslive.com>
2025-08-30 01:35:49 +0300

    Add migration introducing FirstName and LastName to AspNetUsers table.

--- CineTrackPortal/Data/Migrations/20250829221934_AddFirstLastNameToAspNetUser.Designer.cs
+++ CineTrackPortal/Data/Migrations/20250829221934_AddFirstLastNameToAspNetUser.Designer.cs
@@ -4,6 +4,7 @@ using CineTrackPortal.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CineTrackPortal.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250829221934_AddFirstLastNameToAspNetUser")]
+    partial class AddFirstLastNameToAspNetUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
