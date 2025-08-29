104a1ff12d43c59b580069a3547c1eb03f0ad970
alex varypatis <alexvarip@windowslive.com>
2025-08-30 01:06:57 +0300

    Add ActorModel and MovieModel classes

---
+++ CineTrackPortal/Models/ActorModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CineTrackPortal.Models
+{
+    public class ActorModel
+    {
+        [Key]
+        public Guid ActorId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+
+        public ICollection<MovieModel> Movies { get; set; } = new List<MovieModel>();
+    }
+}
---
+++ CineTrackPortal/Models/MovieModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CineTrackPortal.Models
+{
+    public class MovieModel
+    {
+        [Key]
+        public Guid MovieId { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public ICollection<ActorModel> Actors { get; set; } = new List<ActorModel>();
+
+    }
+}
