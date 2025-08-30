e814dc99eaad4cb25fd5eac38ec9ee20aca40fea
alex varypatis <alexvarip@windowslive.com>
2025-08-30 18:39:22 +0300

    Updated default route pattern to focus on the Dashboard controller instead of Home.

--- CineTrackPortal/Program.cs
+++ CineTrackPortal/Program.cs
@@ -34,7 +34,7 @@ namespace CineTrackPortal
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Dashboard/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -47,7 +47,7 @@ namespace CineTrackPortal
             app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
+                pattern: "{controller=Dashboard}/{action=Index}/{id?}")
                 .WithStaticAssets();
             app.MapRazorPages()
                .WithStaticAssets();
